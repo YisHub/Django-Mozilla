a79dbe018f30cdef0b38309e6ec5d840a62726a9
Jesus Flores <jesus14sf@gmail.com>
2023-03-26 17:19:58 -0300

    Actualizacion parte 2

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def mi_vista(request):
+    # Lógica de la vista
+    return HttpResponse("Hola, mundo!")
