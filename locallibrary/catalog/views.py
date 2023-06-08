37c827a3b1507e1f8ed4685869c5bd13f1abe901
Jesus Flores <jesus14sf@gmail.com>
2023-06-07 21:46:41 -0300

    Avances hasta la parte 7 del curso

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -17,10 +17,15 @@ def index(request):
     num_genre=Genre.objects.count()
     num_books_with_Oriente=Book.objects.filter(title__icontains='Oriente').count()
     # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+
+    # Numero de visitas a esta view, como está contado en la variable de sesión.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genre':num_genre, 'num_books_with_Oriente': num_books_with_Oriente},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors, 'num_genre':num_genre, 'num_books_with_Oriente': num_books_with_Oriente, 'num_visits':num_visits},
     )
 
 class BookListView(generic.ListView):
